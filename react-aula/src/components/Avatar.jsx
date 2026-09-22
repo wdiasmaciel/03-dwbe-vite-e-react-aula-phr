fc8c5bb4c9db82207cc2622387ae138b5885cc80
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-09-22 18:07:16 +0000

    Exemplo

--- react-aula/src/components/Avatar.jsx
+++ react-aula/src/components/Avatar.jsx
@@ -1,13 +1,12 @@
 export default function Avatar({ url, nome }) {
   return (
     <>
-      <p>{nome}</p>
-      <p>${nome}</p>
       <img 
         src={url || "https://via.placeholder.com/150"} 
         alt={`Foto de ${nome}`} 
         style={{ borderRadius: '50%', width: 50, height: 50 }} 
       />
+      <p>{nome}</p>
     </>
   );
 }
