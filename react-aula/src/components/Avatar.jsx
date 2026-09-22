cb7a2f4c639f2942eca2bab09179d0483910288d
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-09-22 18:30:11 +0000

    Exemplo

--- react-aula/src/components/Avatar.jsx
+++ react-aula/src/components/Avatar.jsx
@@ -1,8 +1,10 @@
 export default function Avatar({ url, nome }) {
+  let numAleatorio = Math.floor(Math.random() * 200);
+  
   return (
     <>
       <img 
-        src={url || "https://dummyjson.com/icon/abc123/150"} 
+        src={url || `https://dummyjson.com/icon/abc${numAleatorio}/150`} 
         alt={`Foto de ${nome}`} 
         style={{ borderRadius: '50%', width: 50, height: 50 }} 
       />
